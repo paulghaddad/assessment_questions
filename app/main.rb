1f80643b02a06f41eb43f6032b6f1d9843be8d78
Paul Haddad <paulh16@gmail.com>
2017-10-19 07:29:31 -0500

    Add Questions endpoint limit capability

--- app/main.rb
+++ app/main.rb
@@ -7,6 +7,8 @@ require "active_support/core_ext/object/json"
 require File.expand_path("../questions_importer.rb", __FILE__)
 require File.expand_path("../question_store.rb", __FILE__)
 
+QUESTIONS_LIMIT = 100
+
 csv_file = File.open(File.join(File.dirname(__FILE__),
                                "code_challenge_question_dump.csv"))
 questions_import = QuestionsImporter.new(file: csv_file).import_csv
@@ -16,7 +18,9 @@ get "/questions" do
   response["Access-Control-Allow-Origin"] = "*"
   status 200
   content_type :json
-  questions_data_store.all.to_json
+  limit = params["limit"] ? Integer(params["limit"]) : QUESTIONS_LIMIT
+
+  questions_data_store.all(limit: limit).to_json
 end
 
 get "/questions/:id" do
